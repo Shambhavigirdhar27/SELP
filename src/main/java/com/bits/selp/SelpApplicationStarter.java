ab7765e81fd489f2360c334fbe598c8fa95f46f0
Shambhavi Girdhar <shambhavi.girdhar@clayhr.com>
2025-10-30 02:42:56 +0530

    Initial commit for School Equipment Lending Project.

---
+++ src/main/java/com/bits/selp/SelpApplicationStarter.java
@@ -0,0 +1,11 @@
+package com.bits.selp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelpApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(SelpApplicationStarter.class, args);
+    }
+}
